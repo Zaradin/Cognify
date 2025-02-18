8ac869563ef814d1c2da760a3681d16e41edecd1
Zaradin <crottyjj@hotmail.com>
2025-02-18 19:31:11 +0000

    added particle effect to login page

--- frontend/src/components/particles/particles.js
+++ frontend/src/components/particles/particles.js
@@ -1,54 +1,144 @@
-// import { useEffect, useState } from "react";
-// import Particles from "@tsparticles/react";
-// import { initParticlesEngine } from "@tsparticles/react";
-// import { loadSlim } from "@tsparticles/slim"; // Import only the slim version to reduce bundle size
+import { useEffect, useState } from "react";
+import Particles from "@tsparticles/react";
+import { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim"; // Import only the slim version to reduce bundle size
 
-// const ParticlesComponent = () => {
-//     const [init, setInit] = useState(false);
+const ParticlesComponent = () => {
+    const [init, setInit] = useState(false);
 
-//     // This effect runs only once when the component is mounted
-//     useEffect(() => {
-//         initParticlesEngine(async (engine) => {
-//             // Initialize the tsParticles engine and load the slim version
-//             await loadSlim(engine);
-//         }).then(() => {
-//             setInit(true); // Set the particles to load after initialization
-//         });
-//     }, []);
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            await loadSlim(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
 
-//     const particlesLoaded = (container) => {
-//         console.log(container); // Optionally handle particles loaded event
-//     };
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
 
-//     return (
-//         init && (
-//             <Particles
-//                 id="tsparticles"
-//                 particlesLoaded={particlesLoaded}
-//                 options={{
-//                     background: {
-//                         color: {
-//                             value: "#0d47a1", // Background color
-//                         },
-//                     },
-//                     particles: {
-//                         number: { value: 100 },
-//                         size: { value: 3 },
-//                         move: {
-//                             enable: true,
-//                             speed: 1,
-//                         },
-//                         shape: {
-//                             type: "circle",
-//                         },
-//                         color: {
-//                             value: "#ffffff", // Particle color
-//                         },
-//                     },
-//                 }}
-//             />
-//         )
-//     );
-// };
+    return (
+        <>
+            {init && (
+                <Particles
+                    id="tsparticles"
+                    particlesLoaded={particlesLoaded}
+                    options={{
+                        background: {
+                            color: {
+                                value: "#fff",
+                            },
+                        },
+                        fpsLimit: 120,
+                        interactivity: {
+                            events: {
+                                onHover: {
+                                    enable: true,
+                                    mode: "repulse",
+                                },
+                                onClick: {
+                                    enable: true,
+                                    mode: "repulse",
+                                },
+                                resize: true,
+                            },
+                            modes: {
+                                grab: {
+                                    distance: 400,
+                                    links: {
+                                        opacity: 1,
+                                    },
+                                },
+                                bubble: {
+                                    distance: 400,
+                                    size: 40,
+                                    duration: 2,
+                                    opacity: 8,
+                                    speed: 3,
+                                },
+                                repulse: {
+                                    distance: 119.88,
+                                    duration: 0.4,
+                                },
+                                push: {
+                                    quantity: 4,
+                                },
+                                remove: {
+                                    quantity: 2,
+                                },
+                            },
+                        },
+                        particles: {
+                            number: {
+                                value: 160,
+                                density: {
+                                    enable: true,
+                                    area: 500,
+                                },
+                            },
+                            color: {
+                                value: "#000000",
+                            },
+                            shape: {
+                                type: "triangle",
+                                stroke: {
+                                    width: 0,
+                                    color: "#000000",
+                                },
+                                polygon: {
+                                    sides: 5,
+                                },
+                                image: {
+                                    src: "img/github.svg",
+                                    width: 100,
+                                    height: 100,
+                                },
+                            },
+                            opacity: {
+                                value: 0.5,
+                                random: false,
+                                animation: {
+                                    enable: false,
+                                    speed: 1,
+                                    minimumValue: 0.1,
+                                    sync: false,
+                                },
+                            },
+                            size: {
+                                value: 3,
+                                random: true,
+                                animation: {
+                                    enable: true,
+                                    speed: 4,
+                                    minimumValue: 1,
+                                    sync: true,
+                                },
+                            },
+                            links: {
+                                enable: true,
+                                distance: 189.3954,
+                                color: "#b3b3b3",
+                                opacity: 0.5,
+                                width: 1.5783,
+                            },
+                            move: {
+                                enable: true,
+                                speed: 4,
+                                direction: "none",
+                                random: true,
+                                straight: false,
+                                outModes: {
+                                    default: "out",
+                                },
+                            },
+                        },
+                        detectRetina: true,
+                    }}
+                />
+            )}
+        </>
+    );
+};
 
-// export default ParticlesComponent;
+export default ParticlesComponent;
