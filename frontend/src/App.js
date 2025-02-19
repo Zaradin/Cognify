346c329af5c521c2d200c1d6ce9021a70e724d9d
Zaradin <crottyjj@hotmail.com>
2025-02-19 13:33:28 +0000

    patients page boiler plate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import HomePage from "./pages/homePage.js";
 import LoginPage from "./pages/loginPage.js";
 import DashboardPage from "./pages/dashboardPage.js";
+import PatientsPage from "./pages/patientsPage.js";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/dashboard" element={<DashboardPage />} />
+                <Route path="/patients" element={<PatientsPage />} />
             </Routes>
         </BrowserRouter>
     );
